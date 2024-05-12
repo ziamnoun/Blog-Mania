27afd5c60475c6fb41ef4158dccdf2f46d433802
ziamnoun <ziamziam05@gmail.com>
2024-05-12 20:32:14 +0600

    Sample card done

--- src/HomeCard/HomeCard.jsx
+++ src/HomeCard/HomeCard.jsx
@@ -8,10 +8,37 @@ const HomeCard = () => {
 
 
 
+
+      
+
+
+
+
     return (
-        <div>
+        <div className="card w-96 glass bg-black text-white">
+        <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Blog Post Image"/></figure>
+        <div className="card-body">
+          <h2 className="card-title ">Title of the Blog Post</h2>
+          <p>Short Description of the Blog Post. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <div className="flex justify-between items-center mt-4">
+            <div>
+              <p className="text-sm text-red-600">Category: Drawing & Painting</p>
+              <p className="text-sm text-red-600">Date: May 12, 2024</p>
+            </div>
             
+            </div>
+            <div>
+              <button className="btn btn-primary mr-2 bg-red-600">View Details</button>
+              <button className="btn btn-danger border-red-600">Delete</button>
+            </div>
+          
         </div>
+      </div>
+      
+
+      
+      
+
     );
 };
 
