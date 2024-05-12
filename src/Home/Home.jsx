a772941ac620a274716dc1b1f6168b9fba3f9cdf
ziamnoun <ziamziam05@gmail.com>
2024-05-12 20:44:11 +0600

    Card done

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import HomePageWrittenPart from '../HomePageWritten/HomePageWrittenPart';
+import HomeCard from '../HomeCard/HomeCard';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <HomePageWrittenPart></HomePageWrittenPart>
+            <HomeCard></HomeCard>
             
         </div>
     );
--- src/HomeCard/HomeCard.jsx
+++ src/HomeCard/HomeCard.jsx
@@ -1,45 +1,44 @@
+
+
+
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
-const HomeCard = () => {
-    const AllBlogData=useLoaderData();
-    console.log(AllBlogData)
-
-
-
-
-
-      
-
-
-
-
-    return (
-        <div className="card w-96 glass bg-black text-white">
-        <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Blog Post Image"/></figure>
-        <div className="card-body">
-          <h2 className="card-title ">Title of the Blog Post</h2>
-          <p>Short Description of the Blog Post. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <div className="flex justify-between items-center mt-4">
-            <div>
-              <p className="text-sm text-red-600">Category: Drawing & Painting</p>
-              <p className="text-sm text-red-600">Date: May 12, 2024</p>
-            </div>
-            
-            </div>
-            <div>
-              <button className="btn btn-primary mr-2 bg-red-600">View Details</button>
-              <button className="btn btn-danger border-red-600">Delete</button>
-            </div>
-          
+const Homecard = ({ blogData }) => {
+  return (
+    <div className="card w-96 glass bg-black text-white ">
+      <figure><img src={blogData.imageURL} alt="Blog Post Image"/></figure>
+      <div className="card-body">
+        <h2 className="card-title">{blogData.title}</h2>
+        <p>{blogData.shortDescription}</p>
+        <div className="flex justify-between items-center mt-4">
+          <div>
+            <p className="text-sm text-red-600">Category: {blogData.category}</p>
+            <p className="text-sm text-red-600">Date: {blogData.date}</p>
+          </div>
+          <div>
+            <button className="btn btn-primary mr-2 bg-red-600">View Details</button>
+            <button className="btn btn-danger border-red-600">Delete</button>
+          </div>
         </div>
       </div>
-      
+    </div>
+  );
+};
 
-      
-      
+const HomeCard = () => {
+  const allBlogData = useLoaderData();
 
-    );
+ 
+  const latestBlogData = allBlogData.slice(0, 6);
+
+  return (
+    <div className="grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 mt-10 gap-5 md:w-[80%] m-auto">
+      {latestBlogData.map((blogData, index) => (
+        <Homecard key={index} blogData={blogData} />
+      ))}
+    </div>
+  );
 };
 
 export default HomeCard;
